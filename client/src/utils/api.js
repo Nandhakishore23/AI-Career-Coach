7f95d9c0ecaf2b4e8d4728611144c3d64356100f
Nandhakishore A <131781031+Nandhakishore23@users.noreply.github.com>
2026-01-24 09:39:10 +0530

    api update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5005/api',
+    baseURL: 'https://ai-career-coach-backend-one.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
